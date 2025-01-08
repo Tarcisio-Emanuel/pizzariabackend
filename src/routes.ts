78b95358e94bf7c4aaddf2acffc543ad31cccef9
Tarcisio <tarcisioemanoel10031@gmail.com>
2025-01-07 23:16:44 -0300

    commit

--- src/routes.ts
+++ src/routes.ts
@@ -10,11 +10,13 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 
-import uploadConfig from './config/multer';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
-import { AddItemController } from './controllers/order/AdditemController';
+import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrderController } from './controllers/order/ListOrderController';
+
+import uploadConfig from './config/multer';
 
 const router = Router();
 
@@ -40,6 +42,7 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrderController().handle)
 
 
 
