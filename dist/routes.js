df06d94fb4726d58bd9c0f25cb04859cb5c40d00
Tarcisio <tarcisioemanoel10031@gmail.com>
2025-01-12 12:33:03 -0300

    api commit

--- dist/routes.js
+++ dist/routes.js
@@ -28,7 +28,7 @@ const upload = (0, multer_1.default)(multer_2.default.upload("./tmp"));
 //-- ROTAS USER --
 router.post('/users', isAuthenticated_1.isAuthenticated, new CreateUserController_1.CreateUserController().handle);
 // -- ROTA DE LOGIN
-router.post('/session', isAuthenticated_1.isAuthenticated, new AuthUserController_1.AuthUserController().handle);
+router.post('/session', new AuthUserController_1.AuthUserController().handle);
 router.get('/me', isAuthenticated_1.isAuthenticated, new DetailUserController_1.DetailUserController().handle);
 // -- ROTAS CATEGORIA
 router.post('/category', isAuthenticated_1.isAuthenticated, new CreateCategoryController_1.CreateCategoryController().handle);
